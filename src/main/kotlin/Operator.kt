1d426ac11587117174a9daba41122d172f4f3111
Dilip Boss <dilip.web7@gmail.com>
2023-07-26 15:57:09 +0530

    kotlin Pro

---
+++ src/main/kotlin/Operator.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var x=5
+    var y=25
+    println(x + y)
+    println(x -y)
+    println(x*y)
+    println("$x convert into flot "+ x.toFloat())
+
+    println(x>y)
+    println(x>y)
+    println(x >= y)
+    println(x <= y)
+    println(x == y)
+    println("increment "+ x++)//post increment
+    println("decremnt "+ y++)//post increment
+
+    println("increment "+ ++x)//pre increment
+    println("decremnt "+ ++y)//pre increment
+
+
+
+}
