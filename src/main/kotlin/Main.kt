7f05b474ac3a50a5b3650b797cc795aa1331bd08
Dilip Boss <dilip.web7@gmail.com>
2023-07-26 15:35:18 +0530

    kotlin Pro

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
+import org.w3c.dom.Text
+
 fun main(args: Array<String>) {
     println("Hello World!")
+    message("I am Kotlin")
+    print("hello" + 123)
+    print(45.65)
+    println("hello akash "+ 45)
+}
+
+fun message(name: String){
+    println(name)
 }
